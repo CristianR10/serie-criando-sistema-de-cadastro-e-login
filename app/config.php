33833dc7de1ea31d7418be5b465e561c1c3f84de
CristianR10 <cristian_hiei@hotmail.com>
2017-12-14 19:10:01 -0200

    confirmação Documentação

--- app/config.php
+++ app/config.php
@@ -1,77 +1,77 @@
 <?php
-	//Constantes
-	$configs = new HXPHP\System\Configs\Config;
+//Constantes
+$configs = new HXPHP\System\Configs\Config;
+ActiveRecord\Connection::$datetime_format = 'Y-m-d H:i:s';
 
-	$configs->env->add('development');
+$configs->env->add('development');
 
-	$configs->env->development->baseURI = '/sistema/';
+$configs->env->development->baseURI = '/git/local/';
 
-	$configs->env->development->database->setConnectionData(array(
-		'host' => 'localhost',
-		'user' => 'root',
-		'password' => '',
-		'dbname' => 'sistemahx'
-	));
+$configs->env->development->database->setConnectionData(array(
+  'host' => 'localhost',
+  'user' => 'root',
+  'password' => '',
+  'dbname' => 'transporteapp'
+  ));
 
-	$configs->env->development->auth->setURLs('/sistema/home/', '/sistema/login/');
+/*
+  //Globais
+  $configs->title = 'Titulo customizado';
 
-	$configs->env->development->menu->setMenus(array(
-		'Home/dashboard' => '%baseURI%/home',
-		'Editar perfil/cog' => '%baseURI%/perfil/editar',
-		'Sair/sign-out' => '%baseURI%/login/sair',
-	), 'user');
+  //Configurações de Ambiente - Desenvolvimento
+  $configs->env->add('development');
 
-	$configs->env->development->menu->setMenus(array(
-		'Home/dashboard' => '%baseURI%/home',
-		'Usuários/users' => '%baseURI%/usuarios',
-		'Editar perfil/cog' => '%baseURI%/perfil/editar',
-		'Sair/sign-out' => '%baseURI%/login/sair'
-	), 'administrator');
+  $configs->env->development->baseURI = '/hxphp/';
 
-	$configs->env->development->menu->setMenus(array(
-		'Home/dashboard' => '%baseURI%/home'
-	));
+  $configs->env->development->database->setConnectionData([
+  'driver' => 'mysql',
+  'host' => 'localhost',
+  'user' => 'root',
+  'password' => '',
+  'dbname' => 'hxphp',
+  'charset' => 'utf8'
+  ]);
 
-	$configs->env->development->menu->setConfigs(array(
-		'container' => 'nav',
-		'container_class' => 'navbar navbar-default',
-		'menu_class' => 'nav navbar-nav'
-	));
+  $configs->env->development->mail->setFrom([
+  'from' => 'Remetente',
+  'from_mail' => 'email@remetente.com.br'
+  ]);
 
-	$configs->env->add('production');
+  $configs->env->development->menu->setConfigs([
+  'container' => 'nav',
+  'container_class' => 'navbar navbar-default',
+  'menu_class' => 'nav navbar-nav'
+  ]);
 
-	$configs->env->production->baseURI = '/';
+  $configs->env->development->menu->setMenus([
+  'Home/home' => '%siteURL%',
+  'Subpasta/folder-open' => [
+  'Home/home' => '%baseURI%/admin/have-fun/',
+  'Teste/home' => '%baseURI%/admin/index/',
+  ]
+  ]);
 
-	$configs->env->production->database->setConnectionData(array(
-		'host' => 'localhost',
-		'user' => 'hxphp190_sistema',
-		'password' => '^EKI1LK(&kV[',
-		'dbname' => 'hxphp190_sistemahx'
-	));
+  $configs->env->development->auth->setURLs('/hxphp/home/', '/hxphp/login/');
+  $configs->env->development->auth->setURLs('/hxphp/admin/home/', '/hxphp/admin/login/', 'admin');
 
-	$configs->env->production->auth->setURLs('/home/', '/login/');
+  //Configurações de Ambiente - Produção
+  $configs->env->add('production');
 
-	$configs->env->production->menu->setMenus(array(
-		'Home/dashboard' => '%baseURI%/home',
-		'Editar perfil/cog' => '%baseURI%/perfil/editar',
-		'Sair/sign-out' => '%baseURI%/login/sair',
-	), 'user');
+  $configs->env->production->baseURI = '/';
 
-	$configs->env->production->menu->setMenus(array(
-		'Home/dashboard' => '%baseURI%/home',
-		'Usuários/users' => '%baseURI%/usuarios',
-		'Editar perfil/cog' => '%baseURI%/perfil/editar',
-		'Sair/sign-out' => '%baseURI%/login/sair'
-	), 'administrator');
+  $configs->env->production->database->setConnectionData([
+  'driver' => 'mysql',
+  'host' => 'localhost',
+  'user' => 'usuariodobanco',
+  'password' => 'senhadobanco',
+  'dbname' => 'hxphp',
+  'charset' => 'utf8'
+  ]);
 
-	$configs->env->production->menu->setMenus(array(
-		'Home/dashboard' => '%baseURI%/home'
-	));
+  $configs->env->production->mail->setFrom([
+  'from' => 'Remetente',
+  'from_mail' => 'email@remetente.com.br'
+  ]);
+ */
 
-	$configs->env->production->menu->setConfigs(array(
-		'container' => 'nav',
-		'container_class' => 'navbar navbar-default',
-		'menu_class' => 'nav navbar-nav'
-	));
-
-	return $configs;
+return $configs;
